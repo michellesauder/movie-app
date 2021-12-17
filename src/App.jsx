4b885bee3c379dfe4600585c390a9aeec2f77505
Michelle Sauder <mgsauder@hotmail.com>
2021-12-17 11:32:45 -0800

    wip

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import './App.css';
+import * as React from 'react';
+import { Container, Box, InputBase, Card, CardContent, CardActions, Button, Typography} from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+
+
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '50%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '50%',
+    [theme.breakpoints.up('sm')]: {
+      width: '50ch',
+      '&:focus': {
+        width: '50ch',
+      },
+    },
+  },
+}));
+
+function App() {
+  return (<>
+      <Container maxWidth="lg">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', width:'100%', display: 'flex', flexDirection: 'column' }} >
+          <h1>Movie App</h1>
+          <Search>
+            <SearchIconWrapper>
+              {/* <SearchIcon /> */}
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search your favorite movies…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          
+          <Card sx={{ maxWidth: 275 }}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="div">
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                adjective
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Box>
+      </Container>
+
+ </> );
+}
+
+export default App;
